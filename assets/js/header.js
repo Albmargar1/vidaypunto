4e060f6219bee278ce2a1c8ba0ef71d13cceb959
Alberto Martin <albertomge@gmail.com>
2020-05-02 13:05:50 +0200

    Fixed bug with header and footer

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,3 @@
-document.write('<header id="header">');
-document.write('  <div class="inner">');
-document.write('    <a href="../../index.html" class="logo">');
-document.write('      <span class="symbol"><img src="../../images/logo.png" alt="" /></span><span class="headerTitle">Vida y Punto</span>');
-document.write('    </a>');
-document.write('    <nav>');
-document.write('      <ul>');
-document.write('        <li><a href="#menu"></a></li>');
-document.write('      </ul>');
-document.write('    </nav>');
-document.write('  </div>');
-document.write('</header>');
 document.write('<nav id="menu">');
 document.write('  <div class="inner">');
 document.write('    <h2>Menu</h2>');
---
+++ assets/js/logo.js
@@ -0,0 +1,12 @@
+document.write('<header id="header">');
+document.write('  <div class="inner">');
+document.write('    <a href="../../index.html" class="logo">');
+document.write('      <span class="symbol"><img src="../../images/logo.png" alt="" /></span><span class="headerTitle">Vida y Punto</span>');
+document.write('    </a>');
+document.write('    <nav>');
+document.write('      <ul>');
+document.write('        <li><a href="#menu"></a></li>');
+document.write('      </ul>');
+document.write('    </nav>');
+document.write('  </div>');
+document.write('</header>');
