0d575362fa65696d41a961524728b4b433bf3f2a
rvidaurre <raquel0vidaurre@gmail.com>
2020-05-14 19:45:22 +0200

    fixed links

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,7 +3,7 @@ document.write('  <div class="inner">');
 document.write('    <h2>Menu</h2>');
 document.write('    <ul>');
 document.write('      <li><a href="../../index.html">Inicio</a></li>');
-document.write('      <li><a href="../template/template.html">Sobre nosotras</a></li>');
+document.write('      <li><a href="../SobreNosotras/template.html">Sobre nosotras</a></li>');
 document.write('    </ul>');
 document.write('  </div>');
 document.write('</nav>');
