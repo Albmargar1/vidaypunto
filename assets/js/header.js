285ea67833d0956feb4d3c31858818a464925a77
Alberto Martin <albertomge@gmail.com>
2021-03-27 13:18:14 +0100

    Test error 404

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,7 +3,7 @@ document.write('  <div class="inner">');
 document.write('    <h2>Menu</h2>');
 document.write('    <ul>');
 document.write('      <li><a href="../../index.html">Inicio</a></li>');
-document.write('      <li><a href="../SobreNosotras/template.html">Sobre nosotras</a></li>');
+document.write('      <li><a href="../SobreNosotras/main.html">Sobre nosotras</a></li>');
 document.write('      <li><a href="../Patrones/main.html">Patrones</a></li>');
 document.write('      <li><a href="../Portfolio/main.html">Portfolio</a></li>');
 document.write('    </ul>');
