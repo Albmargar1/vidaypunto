a11069352462ec080c8a9aa4d5172470d7107dcd
Alberto Martin <albertomge@gmail.com>
2021-03-27 12:49:48 +0100

    Bug en el menú, no estaba la sección de patrones y portfolio actualizada

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,6 +4,8 @@ document.write('    <h2>Menu</h2>');
 document.write('    <ul>');
 document.write('      <li><a href="../../index.html">Inicio</a></li>');
 document.write('      <li><a href="../SobreNosotras/template.html">Sobre nosotras</a></li>');
+document.write('      <li><a href="../Patrones/main.html">Patrones</a></li>');
+document.write('      <li><a href="../Portfolio/main.html">Portfolio</a></li>');
 document.write('    </ul>');
 document.write('  </div>');
 document.write('</nav>');
