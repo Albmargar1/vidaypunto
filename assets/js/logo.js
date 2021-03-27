f1430db8cf60eb5be570cacf4fda147ac1047eb3
Alberto Martin <albertomge@gmail.com>
2021-03-27 13:52:33 +0100

    Ahora los paths son absolutos :|

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -1,7 +1,7 @@
 document.write('<header id="header">');
 document.write('  <div class="inner">');
-document.write('    <a href="../../index.html" class="logo">');
-document.write('      <span class="symbol"><img src="../../images/logo.png" alt="" /></span><span class="headerTitle">Vida y Punto</span>');
+document.write('    <a href="https://www.vidaypunto.com/index.html" class="logo">');
+document.write('      <span class="symbol"><img src="https://www.vidaypunto.com/images/logo.png" alt="" /></span><span class="headerTitle">Vida y Punto</span>');
 document.write('    </a>');
 document.write('    <nav>');
 document.write('      <ul>');
